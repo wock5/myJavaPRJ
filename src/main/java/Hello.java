ab0a5f5013d772cf4382e7a6bb0fa9f364a8cf7f
wock5 <wwasdasd125@gmail.com>
2024-03-11 15:12:42 +0900

    소스

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
-    public static void main(String[] args){
-        System.out.println("Hello!! My First JAva Coding!!");
+    public static void main(String[] args) {
+
+        System.out.println("Hello!! My First Java Coding!!");
 
         System.out.println("Edit Java Code!!");
     }
