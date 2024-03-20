8d76586a5162710c24ef957c3ba57f033c6d7554
wock5 <wwasdasd125@gmail.com>
2024-03-20 12:47:27 +0900

    24_03_19 밴드 과제

---
+++ src/main/java/Quiz24_03_19.java
@@ -0,0 +1,16 @@
+public class Quiz24_03_19 {
+    public static void main(String[] args) {
+        for (int i = 1 ; i <=7 ; i++) {
+            if(i <=4){
+                for (int j =1 ; j <= i ; j++){
+                    System.out.print("*");
+                }
+            } else {
+                for (int j = 1 ; j <=8-i; j++){
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
