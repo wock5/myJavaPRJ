b85d6a59bc8c1b7faf880c0d2628d1ca67c0af27
wock5 <wwasdasd125@gmail.com>
2024-03-25 18:34:04 +0900

    시험 문제 예상 서식

---
+++ src/main/java/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        String str = "Java";
+
+        System.out.println(str.substring(0,2));
+        System.out.println(str.indexOf("a"));
+    }
+}
