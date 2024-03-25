87c6a0b7dbada8434876108387c38ea92cfcb225
wock5 <wwasdasd125@gmail.com>
2024-03-25 14:57:32 +0900

    공백자르기, 대소문자 변경 모든 공백 제거

--- src/main/java/Code04_13.java
+++ src/main/java/Code04_13.java
@@ -4,14 +4,17 @@ public class Code04_13 {
         String cutStr = "";
         String lower = "";
         String upper = "";
+        String allStr = "";
 
         cutStr = str.trim();
         upper = str.toUpperCase();
         lower = str.toLowerCase();
+        allStr = str.replaceAll(" ","");
 
         System.out.println("기존 문자열 ==> [" + str + "]");
         System.out.println("공백 제거 ==>[" + cutStr + "]");
         System.out.println("대문자 ==> ["+ upper +"]");
         System.out.println("소문자 ==> ["+ lower +"]");
+        System.out.println("모든 공백 제거 ==> ["+ allStr +"]");
     }
 }
