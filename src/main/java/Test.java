9c784acf1c4dd27cbd14a8dc572cad214c488f47
wock5 <wwasdasd125@gmail.com>
2024-03-25 18:48:37 +0900

    시험 문제 예상 서식

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,8 +1,10 @@
 public class Test {
     public static void main(String[] args) {
-        String str = "Java";
+        String str = "Javaclasss";
+        int a = 1;
 
-        System.out.println(str.substring(0,2));
+        System.out.println(str.substring(a+1 ,3));
         System.out.println(str.indexOf("a"));
+        System.out.println(str.indexOf("a",4));
     }
 }
