48beaa6bae2897d4838e4672d390c2ede7525a6e
wock5 <wwasdasd125@gmail.com>
2024-03-29 12:16:54 +0900

    자료형

---
+++ src/main/java/Study_01/Study_01.java
@@ -0,0 +1,15 @@
+package Study_01;
+
+public class Study_01 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!!");
+        System.out.println("안녕하세요");
+        System.out.println(12);
+        System.out.println(-456);
+        System.out.println("3.14");
+        System.out.println("true");
+        System.out.println(false);
+
+        System.out.println(123 + 345);
+    }
+}
