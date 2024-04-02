0ee0b95014246a2a4822b4dab03965e000c2cf04
wock5 <wwasdasd125@gmail.com>
2024-04-02 09:31:00 +0900

    프로그래머스 코딩테스트 문자열 출력하기

---
+++ src/main/java/Study_01/Study_03.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Study_03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+
+        System.out.println(a);
+    }
+}
