7eec5df59ea20763a232e86d58d2cb111ffecc52
wock5 <wwasdasd125@gmail.com>
2024-04-05 12:08:35 +0900

    나도 코딩 상수

---
+++ src/main/java/Study_01/Study_07.java
@@ -0,0 +1,13 @@
+package Study_01;
+
+public class Study_07 {
+    public static void main(String[] args) {
+        final String KR_COUNTRY_CODE = "+82"; // 국가 번호 (빨리) (final=절대 변하지 않는 상수) 상수는 대문자로 만들어 줘야함
+        //KR_COUNTRY_CODE = "+8282";
+        System.out.println(KR_COUNTRY_CODE);
+
+        final double PI = 3.141592; // 원주율
+        final String DATE_OF_BIRTH = "2001-12-31"; //생년월일
+
+    }
+}
