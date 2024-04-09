80bb372a057b4ce58afebfc8839f991306b69cfc
wock5 <wwasdasd125@gmail.com>
2024-04-09 15:47:41 +0900

    소스 업로드

---
+++ src/main/java/Study_01/Code06_06.java
@@ -0,0 +1,11 @@
+package Study_01;
+
+public class Code06_06 {
+    public static void main(String[] args) {
+        int hap = 0;
+        for (int i = 1 ; i <= 10 ; i++) {
+            hap = hap + i;
+        }
+        System.out.println("1부터 10까지의 합계: " + hap);
+    }
+}
