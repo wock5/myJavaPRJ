decb11da263f7229613051e5272be175a459a3e8
wock5 <wwasdasd125@gmail.com>
2024-04-15 14:41:17 +0900

    중첩 for문 구구단 만들기

---
+++ src/main/java/Code06_09_a.java
@@ -0,0 +1,17 @@
+public class Code06_09_a {
+    public static void main(String[] args) {
+
+        for (int k = 2; k<= 9; k++) {
+
+            for (int j= 1; j<= 9; j++)
+                System.out.println(k + " x " + j + " = " + k * j);
+        }
+
+/*        for (int i = 2; i<=3; i++) {
+
+            for (int j= 1; j<= 9; j++) {
+                System.out.println(i + " x " + j + " = " + i * j);
+            }
+        }*/
+    }
+}
