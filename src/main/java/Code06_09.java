1715c895a20e584938c770277207a07c6df34ec1
wock5 <wwasdasd125@gmail.com>
2024-04-15 14:41:40 +0900

    중첩 for문

---
+++ src/main/java/Code06_09.java
@@ -0,0 +1,12 @@
+public class Code06_09 {
+    public static void main(String[] args) {
+
+            for (int i=0; i<3 ; i++) {
+                for (int k=0; k<2 ; k++) {
+                    System.out.println("난생처음은 쉽습니다. ^^" +"(i:"+i+", k:"+k+")");
+                }
+
+                System.out.println(i+ "번째 i 반복문 종료");
+            }
+        }
+    }
